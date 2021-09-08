95ce445c0f2024561f87459de3c06e912955e25f
caio-pc <caiocacador.s@gmail.com>
2021-09-07 22:27:19 -0300

    Added sqs base methods

--- sqs/sqs.py
+++ sqs/sqs.py
@@ -32,15 +32,25 @@ class Message:
 
 class SQS:
 
-    def __init__(self, queue_url: str, application_name: str, aws_region: str):
+    def __init__(self, queue_to_send: str, queue_to_received: str, application_name: str, aws_region: str,
+                 verbose: False):
+        self._verbose = verbose
+        self.__print('[+] Starting SQS...')
+
         self._sqs = boto3.client('sqs', region_name=aws_region)
         self._application_name = application_name
-        self._queue_url = queue_url
+        self._queue_to_send = self._sqs.get_queue_url(QueueName=queue_to_send)
+        self._queue_to_received = self._sqs.get_queue_url(QueueName=queue_to_received)
+        self.__print('[-] Done!')
+
+    def __print(self, _print: str):
+        if self._verbose:
+            print(_print)
 
     def send_message(self, message: dict):
         timestamp = datetime.utcnow().timestamp()
         response = self._sqs.send_message(
-            QueueUrl=self._queue_url,
+            QueueUrl=self._queue_to_send,
             MessageGroupId=self._application_name,
             MessageAttributes={
                 'Author': {
@@ -59,7 +69,7 @@ class SQS:
     def get_message(self, max_number_of_messages: int = 1, visibility_timeout: int = 20, delete_message: bool = False,
                     wait_time: int = 20):
         response = self._sqs.receive_message(
-            QueueUrl=self._queue_url,
+            QueueUrl=self._queue_to_received,
             AttributeNames=['SentTimestamp', 'Author'],
             MaxNumberOfMessages=max_number_of_messages,
             MessageAttributeNames=['All'],
@@ -70,10 +80,10 @@ class SQS:
         if response.get('Messages'):
             for message in response['Messages']:
                 if delete_message:
-                    self.delete_message(message['ReceiptHandle'])
+                    self.delete_message(self._queue_to_received, message['ReceiptHandle'])
             results = [Message(msg) for msg in response['Messages']]
             return results[0] if max_number_of_messages == 1 else results
         return None
 
-    def delete_message(self, receipt_handle):
-        return self._sqs.delete_message(QueueUrl=self._queue_url, ReceiptHandle=receipt_handle)
+    def delete_message(self, queue, receipt_handle):
+        return self._sqs.delete_message(QueueUrl=queue, ReceiptHandle=receipt_handle)
